fde2e4fff1f4d6ced3573bb8c9cd619102a955ca
Gregory Kim <gkim@kimnetics.com>
2023-12-07 11:29:06 -0700

    Add more description to StoneWall problem.

--- Java/Lessons/7 Stacks and Queues/StoneWall.java
+++ Java/Lessons/7 Stacks and Queues/StoneWall.java
@@ -2,41 +2,58 @@
 import java.util.Stack;
 
 class Solution {
-    // 9         d
-    // 8 a a     d e     g
-    // 7 a a   c c c c   g
-    // 6 a a   c c c c   g
-    // 5 a a b b b b b   g
-    // 4 a a b b b b b f g
-    // 3 a a b b b b b f g
-    // 2 a a b b b b b f g
-    // 1 a a b b b b b f g
+    // Stack contains heights and each entry is the top left corner of a rectangle.
+    // Every stack entry represents a rectangle to add to count.
+    //
+    // Loop through heights.
+    //   If current height > last height:
+    //     Push current height to stack as the start of a new rectangle.
+    //   Else:
+    //     Pop rectangles off of stack until we get to a rectangle with a height <= current height.
+    //       For each popped rectangle with a height > current height, increment count.
+    //       If last popped rectangle has a height < current height, put it back on stack.
+    //     Push current height to stack as the start of a new rectangle.
+    //   Update last height.
+    //
+    // The logic produces rectangles that look like the following:
+    //
+    //   9 |          d
+    // h 8 |  a a     d e     g
+    // e 7 |  a a   c c c c   g
+    // i 6 |  a a   c c c c   g
+    // g 5 |  a a b b b b b   g
+    // h 4 |  a a b b b b b f g
+    // t 3 |  a a b b b b b f g
+    // s 2 |  a a b b b b b f g
+    //   1 |  a a b b b b b f g
+    //     +-------------------
+    //             columns
     public int solution(int[] H) {
-        int rectangles = 0;
+        int count = 0;
 
-        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> rectangles = new Stack<>();
 
-        int height = 0;
+        int lastHeight = 0;
         for (int i = 0; i < H.length; i++) {
-            if (H[i] > height) {
-                stack.push(H[i]);
-            } else if (H[i] < height) {
-                while (!stack.empty()) {
-                    int stackHeight = stack.pop();
-                    if (stackHeight == H[i]) {
+            if (H[i] > lastHeight) {
+                rectangles.push(H[i]);
+            } else if (H[i] < lastHeight) {
+                while (!rectangles.empty()) {
+                    int stackHeight = rectangles.pop();
+                    if (stackHeight < H[i]) {
+                        rectangles.push(stackHeight);
                         break;
-                    } else if (stackHeight < H[i]) {
-                        stack.push(stackHeight);
+                    } else if (stackHeight == H[i]) {
                         break;
                     } else {
-                        rectangles++;
+                        count++;
                     }
                 }
-                stack.push(H[i]);
+                rectangles.push(H[i]);
             }
-            height = H[i];
+            lastHeight = H[i];
         }
 
-        return rectangles + stack.size();
+        return count + rectangles.size();
     }
 }
